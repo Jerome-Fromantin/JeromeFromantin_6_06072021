4634302ea0029c7d008cfd77489d6be2ebacafc6
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2021-07-16 17:51:58 +0200

    Ajout de données dynamiques dans la page de photographe

--- scripts-photographe.js
+++ scripts-photographe.js
@@ -1,15 +1,15 @@
-// Récupération des données du fichier JSON.
+// Récupération des données "photographes" du fichier JSON.
 let {photographers} = await fetch("FishEyeData.json").then((res)=>res.json())
 console.log(photographers);
 
 // PAGE DE PHOTOGRAPHE
 // Récupération de l'id du photographe concerné.
 let param = new URLSearchParams(window.location.search);
-let photographerId = param.get("id");
+let thePhotographerId = param.get("id");
 
 // A partir de l'id, récupération des données du photographe concerné.
 function findPhotographer(data) {
-  return data.id == photographerId;
+  return data.id == thePhotographerId;
 }
 let photographer = photographers.find(findPhotographer);
 let photographerName = photographer.name;
@@ -93,7 +93,7 @@ function contactButton(photographerId) {
 function showLeftPart() {
   let sectionleft = document.querySelector("#photo_pres_text");
   let leftPart = fillLeftPart(photographerName, photographerCity, photographerCountry, photographerTagline, photographerTags);
-  let contact = contactButton(photographerId);
+  let contact = contactButton(thePhotographerId);
   sectionleft.appendChild(leftPart);
   sectionleft.appendChild(contact);
 }
@@ -112,10 +112,87 @@ function photoImg(photographerPortrait, photographerName) {
 function showPresent() {
   let presentation = document.querySelector("#photo_pres");
   showLeftPart();
-  //let textPart = showLeftPart(photographerName, photographerCity, photographerCountry, photographerTagline, photographerTags, photographerId);
   let imgPart = photoImg(photographerPortrait, photographerName);
-  //presentation.appendChild(textPart);
   presentation.appendChild(imgPart);
 }
 
 showPresent();
+
+// Récupération des données "médias" du fichier JSON.
+let {media} = await fetch("FishEyeData.json").then((res)=>res.json())
+console.log(media);
+
+// Récupération par filtration des médias du photographe concerné.
+function filterPictures(data) {
+  return data.photographerId == thePhotographerId;
+}
+let pictures = media.filter(filterPictures);
+console.log(pictures);
+
+let mediaImage = pictures[0].image;
+let mediaLikes = pictures[0].likes;
+let mediaTitle = pictures[0].title;
+console.log(mediaImage);
+console.log(mediaLikes);
+console.log(mediaTitle);
+
+// Chaque carte de la page de photographe : Récupère dynamiquement l'image pour le lien.
+function photoPhotoLink(photographerName, mediaImage) {
+  let photoLink = document.createElement("a");
+  photoLink.href = "#";
+  photoLink.className = "dyn_photo_photoLink";
+  photoLink.setAttribute("aria-label", "Photographie");
+  let photoLinkImg = document.createElement("img");
+  photoLinkImg.src = "Images/" + photographerName + "/" + mediaImage;
+  photoLinkImg.className = "dyn_photo_img";
+  photoLink.setAttribute("lang", "en");
+  photoLink.setAttribute("alt", "Value");
+  photoLink.appendChild(photoLinkImg);
+  return photoLink;
+}
+
+// Chaque carte de la page de photographe : Récupère dynamiquement les données pour la ligne de description.
+function photoCardDescr(mediaTitle, mediaLikes) {
+  let description = document.createElement("div");
+  description.className = "photo_card_titleLikes";
+  description.setAttribute("lang", "en");
+  let descriptionTitle = document.createElement("span");
+  descriptionTitle.innerText = mediaTitle;
+  descriptionTitle.className = "dyn_title";
+  descriptionTitle.setAttribute("aria-label", "Titre de la photo");
+  let descriptionLikes = document.createElement("span");
+  descriptionLikes.className = "dyn_likes";
+  descriptionLikes.setAttribute("aria-label", "Likes de la photo");
+  let descriptionLikesNumber = document.createElement("span");
+  descriptionLikesNumber.innerText = mediaLikes;
+  descriptionLikesNumber.setAttribute("aria-label", "Nombre de likes");
+  let descriptionLikesIcon = document.createElement("img");
+  descriptionLikesIcon.src = "Images/Icone-coeur.png";
+  descriptionLikesIcon.className = "icone";
+  descriptionLikes.setAttribute("alt", "Likes");
+  description.appendChild(descriptionTitle);
+  descriptionLikes.appendChild(descriptionLikesNumber);
+  descriptionLikes.appendChild(descriptionLikesIcon);
+  description.appendChild(descriptionLikes);
+  return description;
+}
+
+// Organise en carte toutes les données médias précédemment récupérées.
+function fillArticle() {
+  let fullArticle = document.createElement("article");
+  fullArticle.className = "photo_card";
+  let link = photoPhotoLink(photographerName, mediaImage);
+  let descr = photoCardDescr(mediaTitle, mediaLikes);
+  fullArticle.appendChild(link);
+  fullArticle.appendChild(descr);
+  return fullArticle;
+}
+
+// Montre toutes les cartes remplies dynamiquement.
+function showPhotos() {
+  let section = document.querySelector(".photo_photosLine");
+  for (let picture of pictures) {
+    let article = fillArticle(picture);
+    section.appendChild(article);
+  }
+}
