fb7d5da1aab064e94393e6dabd7436233cc6cba8
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2021-08-25 12:17:39 +0200

    Modification pour réorganisation des fichiers

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: {index: "./scripts-accueil.js", photographe: "./scripts-photographe.js"},
+    entry: {index: "./javascript/scripts-accueil.js", photographe: "./javascript/scripts-photographe.js"},
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js',
