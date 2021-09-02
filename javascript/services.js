94c0ae1a37585a0110cfc9fce8928c3bf90216b4
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2021-09-02 09:18:39 +0200

    Dernières corrections du code en fonction de ESLint

--- javascript/services.js
+++ javascript/services.js
@@ -1,3 +1,4 @@
+/* eslint-disable eqeqeq */
 // Récupère l'intégralité du fichier JSON.
 async function getAll() {
   const data = await fetch('FishEyeData.json').then((res) => res.json());
@@ -13,13 +14,13 @@ export const getPhotographers = async () => {
 // Récupère le photographe concerné par l'id demandé.
 export const getPhotographer = async (id) => {
   const photographers = await getPhotographers();
-  const photographer = photographers.find((data) => data.id === id);
+  const photographer = photographers.find((data) => data.id == id);
   return photographer;
 };
 
 // Récupère les médias concernés par le photographe demandé.
 export const getMediasByPhotographers = async (id) => {
   const { media } = await getAll();
-  const medias = media.filter((data) => data.photographerId === id);
+  const medias = media.filter((data) => data.photographerId == id);
   return medias;
 };
